84a5bb07663791715533945528bacd1b64522923
BoMaassen <bomaassen@live.nl>
2024-11-15 16:57:35 +0100

    deel 2 af

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,34 @@ import tv from './assets/tv.png'
 import {inventory} from "./constants/inventory.js";
 
 function App() {
+    const sorting = inventory.map((inventorie) => {
+        return inventorie
+    })
+
     function youClickedMostSold() {
         console.log("Meest verkocht eerst")
+
+        sorting.sort((a, b) => b.sold - a.sold)
+        console.log(sorting)
     }
 
     function youClickedCheap() {
         console.log("Goedkoopste eerst")
+        sorting.sort((a, b) => a.price - b.price)
+        console.log(sorting)
     }
 
     function youClickedMostSport() {
         console.log("Meest geschikt voor sport eerst")
+        sorting.sort((a, b) => b.refreshRate - a.refreshRate)
+        console.log(sorting)
     }
 
     return (
         <>
-            <h1>Tech it easy dashboard</h1>
-
+            <header>
+                <h1>Tech it easy dashboard</h1>
+            </header>
             <section>
                 <h2>Verkoopoverzicht</h2>
                 <div className="stats">
@@ -47,50 +59,59 @@ function App() {
             </section>
             <section>
                 <h2>Best verkochte tv</h2>
-                <div className="best-sold-block">
+                <div className="best-sold-block tv-block">
                 <span className="tv-img">
-                <img src={tv} alt="Samsung tv"/>
+                <img src={bestSellingTv.sourceImg} alt="Samsung tv"/>
                 </span>
-                    <div className="best-sold-info">
+                    <div className="best-sold-info tv-info">
                         <h3>{nameTv(bestSellingTv)}</h3>
                         <p>{priceConverter(bestSellingTv)}</p>
                         <p>{sizes(bestSellingTv.availableSizes)}</p>
-                        <p><img src={check} alt="check icoon"/> wifi <img src={minus} alt="minus icoon"/> speech <img
-                            src={check} alt="check icoon"/> hdr <img src={check} alt="check icoon"/> bluethoot <img
+                        <p><img className="icon" src={check} alt="check icoon"/> wifi <img className="icon" src={minus} alt="minus icoon"/> speech <img className="icon"
+                            src={check} alt="check icoon"/> hdr <img className="icon" src={check} alt="check icoon"/> bluethoot <img className="icon"
                             src={minus} alt="minus icoon"/> ambilight</p>
                     </div>
                 </div>
             </section>
             <section>
                 <h2>Alle tvs</h2>
+                <div>
+                <button type="button" onClick={youClickedMostSold}>Meest verkocht eerst</button>
+                <button type="button" onClick={youClickedCheap}>Goedkoopste eerst</button>
+                <button type="button" onClick={youClickedMostSport}>Meest geschikt voor sport eerst</button>
+                </div>
+                <div>
+                    {inventory.map((inventorie) => {
+                        return <div key={inventorie.id} className="tv-block">
+                              <span className="tv-img">
+                                  <img src={inventorie.sourceImg} alt="Samsung tv"/>
+                             </span>
+                            <div className="tv-info">
+                                <h3>{nameTv(inventorie)}</h3>
+                                <p>{priceConverter(inventorie)}</p>
+                                <p>{sizes(inventorie.availableSizes)}</p>
+                                <ul>
+                                    {inventorie.options.map((optie) => {
+                                        if (optie.applicable === true) {
+                                            return <li key={optie.name}><img className="icon" src={check} alt="check icoon"/>{optie.name}
+                                            </li>
+                                        } else return <li key={optie.name}><img className="icon" src={minus}
+                                                                                alt="minus icoon"/>{optie.name}
+                                        </li>
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </section>
+            <footer>
                 <ul>
                     {inventory.map((brandNames) => {
                         return <li key={brandNames.type}>{brandNames.brand}</li>
                     })}
                 </ul>
-                <button type="button" onClick={youClickedMostSold}>Meest verkocht eerst</button>
-                <button type="button" onClick={youClickedCheap}>Goedkoopste eerst</button>
-                <button type="button" onClick={youClickedMostSport}>Meest geschikt voor sport eerst</button>
-                <div>
-                    {inventory.map((inventorie) => {
-                        return <div className="best-sold-info" key={inventorie.type}>
-                            <h3>{nameTv(inventorie)}</h3>
-                            <p>{priceConverter(inventorie)}</p>
-                            <p>{sizes(inventorie.availableSizes)}</p>
-                            <ul>
-                                {inventorie.options.map((optie) => {
-                                    if (optie.applicable === true) {
-                                        return <li key={inventorie.type}><img src={check}
-                                                                              alt="check icoon"/>{optie.name}</li>
-                                    } else return <li key={inventorie.type}><img src={minus}
-                                                                                 alt="minus icoon"/>{optie.name}</li>
-
-                                })}
-                            </ul>
-                        </div>
-                    })}
-                </div>
-            </section>
+            </footer>
         </>
     )
 }
